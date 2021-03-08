13e0842e5795fa255b95d0c34da728181d6e8534
Nick Allen <nwestallen@gmail.com>
2021-03-08 16:11:41 -0500

    GET /api/users

--- api/server.js
+++ api/server.js
@@ -4,8 +4,20 @@ const User = require('./users/model');
 const server = express();
 server.use(express.json());
 
+//GET /api/users
+server.get('/api/users', (req, res) => {
+    User.find()
+      .then(users => {
+          res.status(200).json(users);
+      })
+      .catch(err => {
+          res.status(500).json({ message: err.message });
+      });
+});
+
+
 server.use('*', (req, res) => {
     res.status(400).json({ message: 'resource not found in this server'});
-})
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
