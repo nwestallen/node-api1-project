3f9d1c3e034a119a3e818399f2ce3554bc9e7805
Nick Allen <nwestallen@gmail.com>
2021-03-08 17:13:42 -0500

    error handling for POST

--- api/server.js
+++ api/server.js
@@ -34,13 +34,17 @@ server.get('/api/users/:id', (req, res) => {
 //POST /api/users
 server.post('/api/users', (req, res) => {
     const newUser = req.body;
-    User.insert(newUser)
-      .then(user => {
-          res.status(201).json(user);
-      })
-      .catch(err => {
-          res.status(500).json({ message: err.message });
-      });
+    if(!newUser.name || !newUser.bio) {
+        res.status(400).json({ message: 'Please include a name and bio for the user'});
+    } else {
+        User.insert(newUser)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message });
+        });
+    }
 });
 
 server.use('*', (req, res) => {
