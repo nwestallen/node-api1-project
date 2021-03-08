18529d385af72096af19fecfe90fe0b9d7fdfabd
Nick Allen <nwestallen@gmail.com>
2021-03-08 16:05:49 -0500

    imported dependencies and got server running

--- api/server.js
+++ api/server.js
@@ -1,3 +1,11 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
+const server = express();
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use('*', (req, res) => {
+    res.status(400).json({ message: 'resource not found in this server'});
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
